3a52efd7a76765348652ffcde99e1b0b99abb104
Yaz117 <114451792+Yaz117@users.noreply.github.com>
2022-10-10 20:23:25 -0500

    Add files via upload

---
+++ ApliWeb/php/greeting.php
@@ -0,0 +1,25 @@
+<body bgcolor="pink"><?php
+echo "<br>";
+
+$nombre = $_POST['name'];
+echo "<center><h1><i>Holaaa $nombre <33<i></h1>";
+echo "<hr>";
+
+
+
+$edad = $_POST['edad'];
+echo "<center><h1><i>Su edad es $edad <i></h1>";
+echo "<hr>";
+
+
+
+$telefono = $_POST['telefono'];
+echo "<center><h1><i>Su telefono es... $telefono <i></h1>";
+echo "<hr>";
+
+
+
+echo "<a href='../html/greetingForm.html'> Regresar </a></center>";
+
+?>
+</body>
---
+++ ApliWeb/php/interfaz.php
@@ -0,0 +1,60 @@
+<html>
+    <link rel="stylesheet" href="./css/style.css">
+<body bgcolor="pink">
+    <center>
+    <font face="Comic Sans MS,arial,verdana">
+		<label><h2>Programa que manda saludos</h2>
+     <h3>Nombre</h3><input> <button>Saludar</button>
+        
+     <script type="text/javascirpt">
+
+
+     </script>
+<br><br>
+     <table>
+        <tr> 
+            <td> 
+                    Nombre
+            </td>
+            <td>
+                    <input text name="" id="">
+            </td>
+
+            <tr> 
+                <td> 
+                   Edad
+                </td>
+                <td>
+                    <input type="number"  name="" id="">   
+                </td>
+            </tr>
+                <td> 
+                    Correo Electrónico
+                 </td>
+                 <td>
+                     <input type="email"  name="" id="">   
+                 </td>
+            </tr>
+                <td> 
+                    Género
+                </td>
+                <td>
+                <select>
+                <option value="">Selecciona</option>
+                <option value="">Mujer</option>
+                <option value="">Hombre</option>
+                <option value="">Otro</option>
+                </select>   
+            </td>
+            <tr>
+            <td>
+                <button>Saludar</button>
+            </tr>
+            </td>
+     </table>
+     <script src="saludar.js"></script>
+        </font>
+    </center>
+</label>
+</body>
+</html>
